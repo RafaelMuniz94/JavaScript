7d1a3f4a6a83916e89eb0891f8871f980b53cbb1
Raru <raruzh@gmail.com>
2019-06-17 21:19:29 -0300

    Adicao do botao de deletar na todolist

--- Introducao/Modulo 3/todo.js
+++ Introducao/Modulo 3/todo.js
@@ -21,9 +21,23 @@ function adicionarTarefa(tarefa){
 function preencherLista(tarefa){
     
         var linha = document.createElement("li");
+        var span = document.createElement('span');
+        
         var text = document.createTextNode(tarefa);
 
-        linha.appendChild(text);
+        
+        var btnApagar = document.createElement("button");
+        btnApagar.innerText = 'Apagar';
+
+        btnApagar.onclick = function() {
+            tarefas.removeChild(this);
+            todoList.removeChild(this);
+        };
+
+        span.appendChild(text);
+        span.appendChild(btnApagar);
+
+        linha.appendChild(span);
         todoList.appendChild(linha);
-    
+
 }
